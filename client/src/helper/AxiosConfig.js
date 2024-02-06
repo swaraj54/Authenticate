10187f058e9e723262f675f09677413dfdd0b086
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-02-06 19:29:12 +0530

    backend url statically added.

--- client/src/helper/AxiosConfig.js
+++ client/src/helper/AxiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: process.env.BACKEND_URL })
+const api = axios.create({ baseURL: "https://authenticate-server.onrender.com/api/v1" })
 
 export default api;
