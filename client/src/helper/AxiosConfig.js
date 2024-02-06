79a9e4b9eba03fbdefbaffb34c44ebc6469f43ad
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-02-06 19:18:39 +0530

    Backend url added to frontend.

--- client/src/helper/AxiosConfig.js
+++ client/src/helper/AxiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: "http://localhost:8000/api/v1" })
+const api = axios.create({ baseURL: process.env.BACKEND_URL })
 
 export default api;
